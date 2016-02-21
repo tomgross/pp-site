7ab3e1a503c91581ffdcfaccb1e4fe540148d344
Tom Gross <itconsense@gmail.com>
2016-02-21 12:39:10 +0100

    adjust size for menu click

--- src/itc.pptheme/itc/pptheme/static/js/main.js
+++ src/itc.pptheme/itc/pptheme/static/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $(window).resize(function(){
-    if($(window).width() >= 992){
+    if($(window).width() >= 768){
       $('.clickable-dropdown').on('click', function(){
         window.location.href=$(this).attr('href');
       });

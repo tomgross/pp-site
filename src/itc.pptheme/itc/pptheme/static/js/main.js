2f48e3b6eca0f9115a21c3bd58ddec7223df8a3b
Tom Gross <itconsense@gmail.com>
2016-02-21 12:01:47 +0100

    fix click on main menu items

--- src/itc.pptheme/itc/pptheme/static/js/main.js
+++ src/itc.pptheme/itc/pptheme/static/js/main.js
@@ -1,1 +1,9 @@
-$(document).ready(function() { });
+$(document).ready(function(){
+  $(window).resize(function(){
+    if($(window).width() >= 992){
+      $('.clickable-dropdown').on('click', function(){
+        window.location.href=$(this).attr('href');
+      });
+    }
+  }).resize();
+});
